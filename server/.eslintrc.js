beee8b4f37542f22a8bc628691be2a5a8df8f5a5
AmirDebbie <67858073+AmirDebbie@users.noreply.github.com>
2020-08-24 11:30:42 +0300

    finished first get req with query params

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -10,5 +10,6 @@ module.exports = {
     ecmaVersion: 12,
   },
   rules: {
+    'max-len': ['error', { code: 120 }],
   },
 };
--- server/app.js
+++ server/app.js
@@ -1,5 +1,21 @@
 const express = require('express');
-const app = express();
+const fs = require('fs').promises;
 
+const jsonFile = './data.json';
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/api/tickets', async (req, res) => {
+  const winnersLog = await fs.readFile(jsonFile, { encoding: 'utf-8' });
+  if (req.query.searchText) {
+    const filteredList = JSON.parse(winnersLog)
+      .filter((item) => item.title.toLowerCase().indexOf(req.query.searchText.toLowerCase()) !== -1);
+    res.send(JSON.stringify(filteredList));
+  } else {
+    res.send(winnersLog);
+  }
+});
 
 module.exports = app;
